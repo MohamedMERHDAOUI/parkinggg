2f313c12a0d9ec2e7191918d682865771cfa6f4a
karunanayakedinura <dinurakarunanayake0@gmail.com>
2024-03-11 15:45:21 +0100

    Update test.blade.php

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -1,18 +1,36 @@
 <?php
-echo"bonjour jDM"."<br>";
-echo "<body bgcolor='white'>";
-$NbrLigne = 3;
-$NbrCol = 4;
-echo '<table>';
+echo"bonjour jDM";
+echo "<body bgcolor=''>";
+
+$NbrCol = 9;
+$NbrLigne = 9;
+
+echo '<table border="1" width="400">';
+
+   echo '<tr>';
+   echo '<td bgcolor="#CCCCCC">i X j</td>';
+   for ($j=1; $j<=$NbrCol; $j++) {
+      echo '<td bgcolor="#FFFF66">'.$j.'</td>';
+   }
+   echo '</tr>';
+
 for ($i=1; $i<=$NbrLigne; $i++) {
    echo '<tr>';
    for ($j=1; $j<=$NbrCol; $j++) {
-         echo '<td>';
-          // ------------------------------------------
-          // AFFICHAGE ligne $i, colonne $j
-         echo "Salut toi";
-          // ------------------------------------------
-         echo '</td>';
+       
+      if ($j==1) {
+         echo '<td bgcolor="#FFFF66">'.$i.'</td>';
+      }
+       
+         if ($i==$j) {
+            echo '<td bgcolor="#FFCC66">';
+         } else {
+            echo '<td>';
+         }
+         
+      echo $i*$j;
+       
+      echo '</td>';
    }
    echo '</tr>';
    $j=1;
