2915595885c62025b17ee7734f370551cc4603dd
karunanayakedinura <dinurakarunanayake0@gmail.com>
2024-03-11 15:29:42 +0100

    test tableau

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -1,4 +1,20 @@
 <?php
 echo"bonjour jDM";
 echo "<body bgcolor='white'>";
+$NbrLigne = 
+echo '<table>';
+for ($i=1; $i<=$NbrLigne; $i++) {
+   echo '<tr>';
+   for ($j=1; $j<=$NbrCol; $j++) {
+         echo '<td>';
+          // ------------------------------------------
+          // AFFICHAGE ligne $i, colonne $j
+         echo $affichage;
+          // ------------------------------------------
+         echo '</td>';
+   }
+   echo '</tr>';
+   $j=1;
+}
+echo '</table>';
 ?>
