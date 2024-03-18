618477e2e183e9f717e73275dcfa4a5782641366
karunanayakedinura <dinurakarunanayake0@gmail.com>
2024-03-18 10:37:55 +0100

    rename page inscription

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -62,7 +62,7 @@ text-align: center;
 }
 </style>
 <button class="button button3">Accueil</button>
-<a href ="inscription.php "<button class="button button2">Inscription</button></a>
+<a href ="inscription.blade.php"<button class="button button2">Inscription</button></a>
 <button class="button button1">Réserver une place</button>
 <br>
 <?php
